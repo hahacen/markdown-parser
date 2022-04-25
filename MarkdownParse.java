287ae0bf0068755380ddf03f07eb8151cf48d244
hahacen <103203631+hahacen@users.noreply.github.com>
2022-04-24 21:14:38 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -19,6 +19,11 @@ public class MarkdownParse {
 	if(openBracket == -1 || closeBracket == -1 || openParen == -1 || closeParen == -1){
                 break;
             }
+	if(markdown.indexOf("!")+1==markdown.indexOf("["))
+	{
+		break;
+	}
+		
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
 	
