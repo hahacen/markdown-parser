473e3a0b81dcc104f03ee00007bb3c7e84a74c7b
hahacen <103203631+hahacen@users.noreply.github.com>
2022-04-28 08:39:08 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,14 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 public class MarkdownParseTest {
-
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
     }
-
+    @Test
+    public void getLinks() {\
+        //first entry will be what we want them to print
+        //second entry call the getLinks()
+        assertEquals(List.of("https://something.com","some-thing.html"),getLinks((test-file.md).readtostring));
+    }
 }
