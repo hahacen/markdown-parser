3a2837d71c511cd23adcf48ec0fe9fedd3d3cb4a
hahaocen <103203631+hahaocen@users.noreply.github.com>
2022-05-08 15:06:07 -0700

    adding a line

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -5,9 +5,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
+
 public class MarkdownParse {
 
+
     public static ArrayList<String> getLinks(String markdown) {
+
+
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
@@ -27,6 +31,7 @@ public class MarkdownParse {
 
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
+
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
