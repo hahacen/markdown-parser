a94886036546caf27fffb88447afa3aa7c85a02f
hahaocen <103203631+hahaocen@users.noreply.github.com>
2022-05-22 23:37:25 -0700

    a

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,14 @@
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
-
 import org.junit.*;
+
 public class MarkdownParseTest {
+    private static MarkdownParse mark;
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
@@ -18,4 +23,14 @@ public class MarkdownParseTest {
         //assertEquals(List.of("https://something.com","some-thing.html"),getLinks(("test-file.md").readtostring));
 
     }
+    @Test
+    public void testSnippet1() throws IOException{
+        Path fileName=Path.of("Snippe1.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> expected= new ArrayList<>();
+        expected.add("another link");
+        
+        assertEquals(expected,mark.getLinks(content));
+    }
+
 }
