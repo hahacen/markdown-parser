0f701a4f385f43a0acb0c0a5d3b448677db3e1f1
hahaocen <103203631+hahaocen@users.noreply.github.com>
2022-05-23 00:03:24 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -31,6 +31,29 @@ public class MarkdownParseTest {
         expected.add("another link");
         assertEquals(expected,mark.getLinks(content));
     }
-    
+
+    @Test
+    public void testSnippet2() throws IOException{
+        Path fileName=Path.of("/Users/a/Documents/GitHub/markdown-parser/Snippet2.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> expected= new ArrayList<>();
+        expected.add("nested link");
+        expected.add("a nested parenthesized url");
+        expected.add("some escaped [ brackets ]");
+
+        assertEquals(expected,mark.getLinks(content));
+
+}
+
+
+@Test
+    public void testSnippet3() throws IOException{
+        Path fileName=Path.of("/Users/a/Documents/GitHub/markdown-parser/Snippet3.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> expected= new ArrayList<>();
+        expected.add("[this title text is really long and takes up more than one line]");
+        assertEquals(expected,mark.getLinks(content));
+
+}
 
 }
